ba25abcdb8f71bed259f49018f5c8672b1695101
Nils Kolvenbach <info@nils-kolvenbach.de>
2022-04-02 15:30:29 +0200

    Testing camera

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get upgrade -y 
-sudo apt-get install python3-picamera
+#sudo apt-get install python3-picamera
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 
 cp ./start-camera.sh /etc/init.d/start-camera.sh
 sudo chmod +x /etc/init.d/start-camera.sh
-sudo update-rc.d start-camera.sh defaults
+sudo update-rc.d start-camera defaults
+sudo reboot
